10ee2fc21a3f91a7741eeb2448c1099d6ae8c5b4
Saif M <sikhlana@gmail.com>
2020-09-15 06:38:56 +0600

    Ready for prime time

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -ex
 
 composer run post-autoload-dump --ansi --no-interaction
 php artisan config:cache --ansi --no-interaction
-php artisan route:cache --ansi --no-interaction
+php artisan route:cache --ansi --no-interaction || true
 php artisan view:cache --ansi --no-interaction
 
 php artisan migrate --force --ansi --no-interaction
 
-php-fpm --fpm-config /php-fpm.conf
+php artisan scheduler:work --ansi --no-interaction
---
+++ app/Console/Commands/Scheduler/WorkCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands\Scheduler;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+class WorkCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scheduler:work';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start a worker that runs the scheduler every minute';
+
+    protected $quit = false;
+
+    /**
+     * Execute the console command.
+     *
+     * @param PhpExecutableFinder $finder
+     * @return int
+     */
+    public function handle(PhpExecutableFinder $finder)
+    {
+        $this->registerSignalHandler();
+        $executable = $finder->find(false);
+
+        do {
+            $process = new Process([
+                $executable,
+                'artisan',
+                'schedule:run',
+            ], base_path());
+
+            $process->start(function ($type, $data) {
+                $this->getOutput()->write($data);
+            });
+
+            usleep(60000000);
+        } while(! $this->quit);
+
+        return 0;
+    }
+
+    protected function registerSignalHandler()
+    {
+        pcntl_async_signals(true);
+
+        pcntl_signal(SIGTERM, function () {
+            $this->quit = true;
+
+            exit(0);
+        });
+    }
+}
